995e8e38d38dcb18b014340e52bd4dd9b2f1e28f
jeffyjo <jeff_1992_1992@hotmail.com>
2017-09-24 16:59:38 +0200

    firebase hosting

--- src/app/pipes/sorting-pipe.ts
+++ src/app/pipes/sorting-pipe.ts
@@ -12,27 +12,27 @@ export class SortingPipe implements PipeTransform {
 
     transform(value: any, type: string){
         this.test[0] = value[0][type]
-        for(let i of value){
-            if(this.test !== undefined){
-                if( this.test.find(i[type]) == -1 ){
-        //             this.test.push(i)
-                }
-                console.log(i[type])
-            }
-            console.log(this.test)
-        }
+        // for(let i of value){
+        //     if(this.test !== undefined){
+        //         if( this.test.find(i[type]) == -1 ){
+        // //             this.test.push(i)
+        //         }
+        //         console.log(i[type])
+        //     }
+        //     console.log(this.test)
+        // }
 
-        this.categoryList.forEach(cat => {
-            for(let item of value){
-                if( item[type] === cat ) {
-                    this.sortedList.push(item)
-                }
-                // console.log(item[type])
-                // go through list and find lowest value
-            }
-        });
+        // this.categoryList.forEach(cat => {
+        //     for(let item of value){
+        //         if( item[type] === cat ) {
+        //             this.sortedList.push(item)
+        //         }
+        //         // console.log(item[type])
+        //         // go through list and find lowest value
+        //     }
+        // });
         console.log(this.sortedList)
-        return this.sortedList
+        return value
     }
 
 }
